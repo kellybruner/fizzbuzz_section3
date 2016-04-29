a70b71b591868a51026dadef72786700f6505e03
Kelly Bruner <brunerka@dukes.jmu.edu>
2016-04-29 14:31:16 -0400

    Update fizzbuzz_section3.js

--- fizzbuzz_section3.js
+++ fizzbuzz_section3.js
@@ -6,23 +6,16 @@ count = function(begin, end) {
 		output = output + " " + buzzer(fizzer(fizzbuzzer(i + 1)));
 	}
 
-	// return, e.g. "1 2 3 4 5"
 	return output;
 };
 
 fizzer = function(num) {
-// 	if(0 === num % 3) {
-// 	return "fizz";
-// }
-// 	return num;
+
 	return 0 === num % 3 ? "fizz" : num;
 };
 
 buzzer = function(num) {
-// 	if(0 === num % 5) {
-// 	return "buzz";
-// }
-// 	return num;
+
 	return 0 === num % 5 ? "buzz" : num;
 };
 
